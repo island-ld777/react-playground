b85d8cce3ef74ae51959d2bb1e70e9e5886948a0
island-ld777 <ldiazh777@gmail.com>
2025-07-07 20:36:30 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { UserProfile } from "./components/UserProfile";
+
+export default function App() {
+    return (
+    <div>
+        <h1>Hello from React! I am in the App Component!</h1>
+        <UserProfile></UserProfile>
+    </div>
+    );
+}
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,20 @@
+export function UserProfile() {
+    return (
+        <div id="user-profile">
+            <h2>User Profile</h2>
+            <p>Username: </p>
+            <p>E-Mail: </p>
+            <ul>
+                <li>
+                    <span>Spam</span>
+                </li>
+                <li>
+                    <span>Sword</span>
+                </li>
+                <li>
+                    <span>Magic Staff</span>
+                </li>
+            </ul>
+        </div>
+    );
+}
